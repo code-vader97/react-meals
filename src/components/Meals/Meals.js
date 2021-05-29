eddf7c6a9cbc000736d3636925d57dc1e2217604
cyb-shashank <dev_7@cybintsolutions.com>
2021-05-30 01:00:44 +0530

    complete working dummy  app

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import MealsSummary from "./MealsSummary";
+import AvailabeMeals from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailabeMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
